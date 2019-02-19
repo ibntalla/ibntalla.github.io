57d41e55c9f2771e94ef139ef77c10bd6b718ad3
domoutalla <33329431+ibntalla@users.noreply.github.com>
2019-02-19 17:26:07 +0000

    second submit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div
+    style="margin-top: 150px"
+    class="text-center cover-container d-flex w-100 h-100 p-3 mx-auto flex-column"
+  >
+    <main role="main" class="inner cover">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-lg-6">
+            <img
+              class="rounded-circle"
+              src="@/public/me.jpg"
+              style="width: 180px"
+            />
+            <h1 class="cover-heading">
+              <font style="vertical-align: inherit;">
+                <font style="vertical-align: inherit;"
+                  >Couvrez votre page.</font
+                ></font
+              >
+            </h1>
+            <p class="lead">
+              <font style="vertical-align: inherit;"
+                ><font style="vertical-align: inherit;"
+                  >Je m'appelle Mamadou Dia, je suis developper frontent. </font
+                ><font style="vertical-align: inherit;"
+                  >Suivez moi sur
+                  <a href="https://github.com/ibntalla">github</a>.</font
+                ></font
+              >
+            </p>
+          </div>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {}
+};
+</script>
